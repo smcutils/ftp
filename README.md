52245e1c9cd93c86d8b80906fa412d8f076e277c
SMC <correo@sergiomarco.com>
2018-02-15 16:37:22 +0100

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ftp
+# ftp [Python scripts to make ftp operations easier]
 
 Making python-ftp capabilities even easier
 
@@ -6,4 +6,8 @@ Making python-ftp capabilities even easier
 
 A simple test connection to ftp.es.debian.org.
 
-The connection parameters are stored in ftp_connection_params.yml file.
+Test/default connection parameters are stored in ftp_connection_params.yml file.
+
+## ftp_list_remote_dir.py
+
+List filenames in specified remote dirs
--- ftp_list_remote_dir.py
+++ ftp_list_remote_dir.py
@@ -1,7 +1,18 @@
 from ftplib import FTP
 import yaml
+import sys
 
-config_data = yaml.load(open('ftp_connection_params.yml', 'r'))
+# Jenkins execution; second parameter
+if len(sys.argv) == 2:
+    config_data = str(sys.argv[1])
+
+#Console execution with params; first parameter
+elif len(sys.argv) == 1:
+    config_data = str(sys.argv[0])
+
+#If no params, test execution to ftp.es.debian.org
+elif len(sys.argv) == 0:
+    config_data = yaml.load(open('ftp_connection_params.yml', 'r'))
 
 ftp = FTP(config_data['ftp_host'])
 ftp.login(config_data['ftp_username'], config_data['ftp_password'])
